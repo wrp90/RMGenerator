c608230e923d2fa0e9d7815a47883aabb01803f3
Will Papp <wpapp90@gmail.com>
2021-03-04 15:53:18 -0600

    added questions prompt and changed const to prompt()

--- index.js
+++ index.js
@@ -1,7 +1,57 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+    .prompt([{
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'What is your description of your project?',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Please enter a Table of Contents.',
+        name: 'TOC',
+    },
+    {
+        type: 'input',
+        message: 'What are your projects install instructions?',
+        name: 'install',
+    },
+    {
+        type: 'input',
+        message: 'How do you use your project?',
+        name: 'usage'
+    },
+    {
+        type: 'checkbox',
+        message:'Choose a license: ' ,
+        choices: [
+            'MIT',
+            'Apache',
+            'MPL',
+            'GPL',
+            'None',
+        ],
+        name: 'License',
+    },
+    {
+        type: 'input',
+        message: 'Would you like to list any contributers?',
+        name: 'contributing',
+    },
+    {
+        type: 'input',
+        message: 'Please write any tests for your project.',
+        name: 'tests',
+    }
+    ]);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
